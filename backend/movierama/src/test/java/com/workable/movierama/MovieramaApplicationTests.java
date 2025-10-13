20655bc4ad0ee3b21a7eead850a1851e60a86419
Pavlos Smith <paulsmith4561@gmail.com>
2025-10-14 01:11:44 +0300

    Initialized spring project

---
+++ backend/movierama/src/test/java/com/workable/movierama/MovieramaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workable.movierama;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieramaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
