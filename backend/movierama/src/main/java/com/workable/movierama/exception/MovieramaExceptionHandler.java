59d28dc8227f726dcb88695e503ed8864d743c0c
Pavlos Smith <paulsmith4561@gmail.com>
2025-10-16 23:35:10 +0300

    Improved error handling

--- backend/movierama/src/main/java/com/workable/movierama/exception/MovieramaExceptionHandler.java
+++ backend/movierama/src/main/java/com/workable/movierama/exception/MovieramaExceptionHandler.java
@@ -1,7 +1,7 @@
 package com.workable.movierama.exception;
 
 import com.workable.movierama.dto.ErrorDTO;
-import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.data.mapping.PropertyReferenceException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -11,23 +11,17 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class MovieramaExceptionHandler {
 
   @ExceptionHandler(MovieramaNotFoundException.class)
-  public ResponseEntity<ErrorDTO> handleNotFound(
-      MovieramaNotFoundException ex, HttpServletRequest request) {
+  public ResponseEntity<ErrorDTO> handleNotFound(MovieramaNotFoundException ex) {
 
     return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorDTO(ex.getMessage()));
   }
 
-  @ExceptionHandler(MovieramaBaseException.class)
-  public ResponseEntity<ErrorDTO> handleBase(
-      MovieramaBaseException ex, HttpServletRequest request) {
-
-    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorDTO(ex.getMessage()));
-  }
-
-  @ExceptionHandler(Exception.class)
-  public ResponseEntity<ErrorDTO> handleGeneric(Exception ex, HttpServletRequest request) {
-
-    return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-        .body(new ErrorDTO(ex.getMessage()));
+  @ExceptionHandler({
+    MovieramaBaseException.class,
+    PropertyReferenceException.class,
+    IllegalArgumentException.class
+  })
+  public ResponseEntity<ErrorDTO> handleBadRequest(Exception ex) {
+    return ResponseEntity.badRequest().body(new ErrorDTO(ex.getMessage()));
   }
 }
